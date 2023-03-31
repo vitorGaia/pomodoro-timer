29e3bb93fecf13f53200ac36a6e515eb6b90caa2
vitorGaia <vitorgaia0027@gmail.com>
2023-03-30 22:57:38 -0300

    aplicação estilizada e funcional, sujeita a mudanças de estilo

--- box-timer/src/Timer.jsx
+++ box-timer/src/Timer.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './Timer.css'
+import sound from './audio/gongoAudio.mp3';
 
 class Timer extends Component {
   state = {
     inputHours: '',
     inputMinutes: '',
     inputSeconds: '',
-    display: '00:00:00'
+    display: '00 : 00 : 00',
+    itsTime: false,
   }
 
   handleChange = ({target}) => {
@@ -22,9 +24,18 @@ class Timer extends Component {
   }
 
   timer = (duration) => {
+    const audio = new Audio(sound);
+    audio.play();
+    this.setState({
+      display: '00 : 00 : 00',
+      itsTime: true,
+      inputHours: '',
+      inputMinutes: '',
+      inputSeconds: '',
+    });
     let timer = duration;
 
-    setInterval(() => {
+    const test = setInterval(() => {
       let hoursCalc = Math.floor((timer / 60) / 60);
       let minutesCalc = Math.floor((timer / 60));
       let secondsCalc = Math.floor(timer % 60);
@@ -35,73 +46,82 @@ class Timer extends Component {
 
       this.setState({
         display: `${hoursCalc} : ${minutesCalc} : ${secondsCalc}`,
-        inputHours: '',
-        inputMinutes: '',
-        inputSeconds: '',
       });
 
       timer -= 1;
 
-      if (timer < 0) {
+      if (timer <= 0) {
         this.setState({
-          display: 'ACABOU!',
+          display: 'E N D !',
+          itsTime: false,
         })
+        audio.play();
+        clearInterval(test);
       }
     }, 1000);
   }
 
   render() {
-    const { inputHours, inputMinutes, inputSeconds, display } = this.state;
+    const { inputHours, inputMinutes, inputSeconds, display, itsTime } = this.state;
+    const backgroundStyle = { backgroundImage: itsTime && "url('https://thumbs.gfycat.com/InfiniteMajorEarthworm-size_restricted.gif')" };
+    const fontColorStyle = { color: itsTime && '#a51b0b' };
+    const fontColorStyle2 = { color: itsTime && '#171820' };
+    const displayNone = { display: itsTime && 'none' }
+
     return (
       <main>
-        <div>
-          <p>{ display }</p>
-        </div>
+        <div className='div-background' style={backgroundStyle}>
+          <div className="show-count-container">
+            <p style={fontColorStyle}>{ display }</p>
+          </div>
 
-        <div>
-          <label>
-            Hours
-            <input
-              type="number"
-              name="inputHours"
-              max="24"
-              min="0"
-              onChange={this.handleChange}
-              value={inputHours}
-            />
-          </label>
+          <div className='control-panel'>
+            <div className='inputs-container' style={displayNone}>
+              <label style={fontColorStyle2}>
+                Hours
+                <input
+                  type="number"
+                  name="inputHours"
+                  max="24"
+                  min="0"
+                  onChange={this.handleChange}
+                  value={inputHours}
+                />
+              </label>
 
-          <label>
-            Minutes
-            <input
-              type="number"
-              name="inputMinutes"
-              max="59"
-              min="0"
-              onChange={this.handleChange}
-              value={inputMinutes}
-            />
-          </label>
+              <label style={fontColorStyle2}>
+                Minutes
+                <input
+                  type="number"
+                  name="inputMinutes"
+                  max="59"
+                  min="0"
+                  onChange={this.handleChange}
+                  value={inputMinutes}
+                />
+              </label>
 
-          <label>
-            Seconds
-            <input
-              type="number"
-              name="inputSeconds"
-              max="59"
-              min="0"
-              onChange={this.handleChange}
-              value={inputSeconds}
-            />
-          </label>
-        </div>
+              <label style={fontColorStyle2}>
+                Seconds
+                <input
+                  type="number"
+                  name="inputSeconds"
+                  max="59"
+                  min="0"
+                  onChange={this.handleChange}
+                  value={inputSeconds}
+                />
+              </label>
+            </div>
 
-        <div>
-          <button>1x Round</button>
+            <div className='buttons-container' style={displayNone}>
+              <button onClick={() => this.timer(300)} style={fontColorStyle2}>1x Round - 5m</button>
 
-          <button>2x Rounds</button>
+              <button onClick={() => this.timer(600)} style={fontColorStyle2}>2x Rounds - 10m</button>
 
-          <button id="btn-go" onClick={this.goCountdown}>GO!</button>
+              <button id="btn-go" onClick={this.goCountdown} style={fontColorStyle2}>It's Time!</button>
+            </div>
+          </div>
         </div>
       </main>
     );
